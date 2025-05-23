6c08d597c6bc2c4ebbda4cc7a31097db0d61758b
Farmaan Elahi <devfarmaan@gmail.com>
2025-05-24 03:43:11 +0530

    Remove duckdb

--- utils/duckdb.tsx
+++ utils/duckdb.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { getDuckDB, initializeDuckDb } from "duckdb-wasm-kit";
-import {
-  AsyncDuckDB,
-  AsyncDuckDBConnection,
-  DuckDBConfig,
-} from "@duckdb/duckdb-wasm";
-
 const TABLES = {
-  symbols:
-    "https://objectstorage.ap-hyderabad-1.oraclecloud.com/n/axbaetdfzydd/b/terminal-files/o/symbols-full-v2.parquet",
-  columns:
-    "https://raw.githubusercontent.com/FarmaanElahi/terminal-data/refs/heads/main/data/scan/cols.parquet",
+  symbols: "symbols",
 };
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 interface QueryProps {
   columns?: string[];
@@ -29,19 +14,14 @@ interface QueryProps {
   offset?: number;
 }
 
-async function simpleQuery(
-  db: AsyncDuckDB,
-  table: keyof typeof TABLES,
-  props?: QueryProps,
-) {
-  let conn: AsyncDuckDBConnection | null = null;
+async function simpleQuery(table: keyof typeof TABLES, props?: QueryProps) {
   const { columns = [], where, order = [], limit, offset } = props ?? {};
   if (columns.length === 0) {
   }
 
   // Columns Builder
   let colsString = columns
-    .map((c) => `"${c}"`)
+    .map((c) => `${c}`)
     .join(",")
     .trim();
   if (!colsString) colsString = "*";
@@ -78,126 +58,19 @@ async function simpleQuery(
   ]
     .filter((p) => p)
     .join(" ");
-  try {
-    conn = await db.connect();
-    return await fetch("http://localhost:8000/query", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query }),
-    });
-  } finally {
-    await conn?.close();
-  }
-}
 
-async function init() {
-  const config: DuckDBConfig = {
-    query: {
-      /**
-       * By default, int values returned by DuckDb are Int32Array(2).
-       * This setting tells DuckDB to cast ints to double instead,
-       * so they become JS numbers.
-       */
-      castBigIntToDouble: true,
-      castDecimalToDouble: true,
-      castTimestampToDate: true,
-    },
-    filesystem: {
-      allowFullHTTPReads: true,
-      reliableHeadRequests: false,
-    },
-  };
-  const db = await initializeDuckDb({
-    config,
-    // debug: process.env.NEXT_PUBLIC_ENVIRONMENT === "development",
-    debug: false,
+  return await fetch(`${API_BASE_URL}/scanner/scan`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
   });
-  await initTables(db);
-  return db;
-}
-
-async function initTables(db: AsyncDuckDB) {
-  let conn: AsyncDuckDBConnection | null = null;
-  try {
-    conn = await db.connect();
-    await conn?.query("SET default_null_order = 'nulls_last';");
-  } finally {
-    await conn?.close();
-  }
-}
-
-function buildQuery(db: AsyncDuckDB) {
-  return async (table: keyof typeof TABLES, props?: QueryProps) => {
-    let conn: AsyncDuckDBConnection | null = null;
-    try {
-      conn = await db.connect();
-      return await simpleQuery(db, table, props);
-    } finally {
-      await conn?.close();
-    }
-  };
-}
-
-type DuckDBContextProps =
-  | { status: "loading" }
-  | {
-      status: "loaded";
-      db: AsyncDuckDB;
-      runQuery: (
-        table: keyof typeof TABLES,
-        props?: QueryProps,
-      ) => Promise<Response>;
-    }
-  | { status: "error"; error: unknown };
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-const DuckDBContext = createContext<DuckDBContextProps>();
-
-export function useDuckDB() {
-  const data = useContext(DuckDBContext);
-  if (!data) {
-    throw new Error("Please specify a duckDB context.");
-  }
-  return data;
 }
 
 export async function queryDuckDB<T>(
   table: keyof typeof TABLES,
   props?: QueryProps,
 ) {
-  const db = await getDuckDB();
-  let conn: AsyncDuckDBConnection | null = null;
-  try {
-    conn = await db.connect();
-    return await simpleQuery(db, table, props).then(
-      (r) => r.json() as Promise<T[]>,
-    );
-  } finally {
-    await conn?.close();
-  }
-}
-
-export function DuckDBProvider({ children }: { children: ReactNode }) {
-  const [db, setDb] = useState<DuckDBContextProps>(() => ({
-    status: "loading",
-  }));
-  useEffect(() => {
-    init()
-      .then((db) =>
-        setDb({
-          status: "loaded",
-          db,
-          runQuery: buildQuery(db),
-        }),
-      )
-      .catch((err) => {
-        console.error(err);
-        setDb({ status: "error", error: err });
-      });
-  }, []);
-
-  return <DuckDBContext.Provider value={db}>{children}</DuckDBContext.Provider>;
+  return await simpleQuery(table, props).then((r) => r.json() as Promise<T[]>);
 }
 
 export async function runRawSymbolQuery(queryBuilder: (tbl: string) => string) {
@@ -205,7 +78,7 @@ export async function runRawSymbolQuery(queryBuilder: (tbl: string) => string) {
   try {
     // const db = await getDuckDB();
     // conn = await db.connect();
-    const rows = await fetch("http://localhost:8000/query", {
+    const rows = await fetch(`${API_BASE_URL}/scanner/scan`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ query: queryBuilder(`'${TABLES.symbols}'`) }),
@@ -218,12 +91,8 @@ export async function runRawSymbolQuery(queryBuilder: (tbl: string) => string) {
 
 export async function runRawSymbolCount(filterSql: string) {
   const sql = `SELECT COUNT(*) as total FROM '${TABLES.symbols}' ${filterSql}`;
-  // let conn: AsyncDuckDBConnection | null = null;
   try {
-    // const db = await getDuckDB();
-    // conn = await db.connect();
-    // const res = await conn.query(sql);
-    const rows = await fetch("http://localhost:8000/query", {
+    const rows = await fetch(`${API_BASE_URL}/scanner/scan`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ query: sql }),
@@ -233,8 +102,6 @@ export async function runRawSymbolCount(filterSql: string) {
     return data[0].total as number;
   } catch (e) {
     console.error("Failed to get the row count", e);
-    return undefined;
-  } finally {
-    // await conn?.close();
+    return 0;
   }
 }
